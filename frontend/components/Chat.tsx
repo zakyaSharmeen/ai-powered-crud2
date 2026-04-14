acda372712ad38958a7248d6503f9cddf3145349
zakyaSharmeen <zakyasharmeen@gmail.com>
2026-04-15 01:43:27 +0530

    added

--- frontend/components/Chat.tsx
+++ frontend/components/Chat.tsx
@@ -66,7 +66,7 @@ export default function Chat({ setTodos }: Props) {
         />
         <button
           onClick={handleSend}
-          className="bg-green-500 px-4 rounded-lg cursor-pointer">
+          className="bg-green-500 px-4 rounded-lg cursor-pointer text-white">
           Send
         </button>
       </div>
